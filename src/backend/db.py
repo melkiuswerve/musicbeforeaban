1405efc7d09a483159c3f4d58c82338bb162819c
swerwe <artem.chet07@mail.ru>
2026-07-04 14:12:41 +0300

    first commit

--- src/backend/db.py
+++ src/backend/db.py
@@ -1,16 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from src.core.settings import DATABASE_URL
 
-DATABASE_URL = "postgresql+asyncpg://music_user:music123@localhost/music"
-ASYNC_DB  = "postgresql://music_user:music123@localhost/music"
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=True)
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-class Base(DeclarativeBase):
-    pass
+
 
 async def get_db() -> AsyncSession:
     async with async_session_maker() as session:
 
         yield session
+
+
+class Base(DeclarativeBase):
+    pass
